f0efceb07fd6a1c454bc04cd6bee74adeabdaa33
Eldubious <eldubious03@gmail.com>
2025-06-26 22:13:46 -0500

    dungeon enemy variety

--- kubejs/server_scripts/server_commands/randomize_items.js
+++ kubejs/server_scripts/server_commands/randomize_items.js
@@ -38,16 +38,38 @@ function randomPositiveEffect(difficulty) {
 
 /*
     Armor constants
-    a list with ['namespace', 'armor type'] for armor and ['lvl', 'enchantment'] for enchantments
+    a list with ['namespace', 'armor type', 'add trims'] for armor and ['lvl', 'enchantment'] for enchantments
 */
-const basic_armor = {0: ['minecraft', 'diamond'], 1: ['immersive_armors', 'warrior']}
-const advanced_armor = {0: ['minecraft', 'diamond'], 1: ['minecraft', 'netherite']}
+const basic_armor = {0: ['minecraft', 'diamond', true], 1: ['immersive_armors', 'warrior', false], 2: ['minecraft', 'iron', true],
+    3: ['call_of_yucutan', 'jades', true], 4: ['twilightforest', 'knightmetal', true], 5: ['immersive_armors', 'heavy', false],
+    6: ['aether', 'phoenix', true], 7: ['aether', 'obsidian', true], 8: ['aether', 'gravitite', true], 9: ['samurai_dynasty', 'white_samurai', false],
+    10: ['samurai_dynasty', 'diamond_samurai', false], 11: ['samurai_dynasty', 'iron_samurai', false], 12: ['samurai_dynasty', 'steel', false],
+    13: ['samurai_dynasty', 'red_samurai', false], 14: ['samurai_dynasty', 'blue_samurai', false], 15: ['samurai_dynasty', 'green_samurai', false],
+    16: ['samurai_dynasty', 'gray_samurai', false], 17: ['iceandfire', 'frost_troll_leather', false], 18: ['iceandfire', 'mountain_troll_leather', false],
+    19: ['iceandfire', 'forest_troll_leather', false], 20: ['iceandfire', 'armor_bronze', false], 21: ['iceandfire', 'armor_silver', false],
+    22: ['iceandfire', 'armor_sapphire', false], 23: ['minecraft', 'diamond', true], 24: ['minecraft', 'diamond', true],
+    25: ['minecraft', 'diamond', true], 26: ['minecraft', 'diamond', true], 27: ['minecraft', 'diamond', true],
+    28: ['minecraft', 'iron', true], 29: ['minecraft', 'iron', true], 30: ['minecraft', 'iron', true]}
+const advanced_armor = {0: ['minecraft', 'diamond', true], 1: ['minecraft', 'netherite', true], 2: ['forbidden_arcanus', 'tyr', false],
+    3: ['call_of_yucutan', 'jades', true], 4: ['twilightforest', 'knightmetal', true], 5: ['forbidden_arcanus', 'draco_arcanus', false],
+    6: ['aether', 'phoenix', true], 7: ['aether', 'obsidian', true], 8: ['aether', 'gravitite', true], 9: ['samurai_dynasty', 'netherite_samurai', false],
+    10: ['samurai_dynasty', 'diamond_samurai', false], 11: ['samurai_dynasty', 'amethyst_samurai', false], 12: ['samurai_dynasty', 'quartz_samurai', false],
+    13: ['samurai_dynasty', 'red_samurai', false], 14: ['samurai_dynasty', 'blue_samurai', false], 15: ['samurai_dynasty', 'green_samurai', false],
+    16: ['samurai_dynasty', 'gray_samurai', false], 17: ['iceandfire', 'dragonsteel_ice', false], 18: ['iceandfire', 'dragonsteel_fire'],
+    19: ['iceandfire', 'dragonsteel_lightning', false], 20: ['iceandfire', 'armor_red', false], 21: ['iceandfire', 'armor_amythest', false],
+    22: ['iceandfire', 'armor_green', false], 23: ['minecraft', 'netherite', true], 24: ['minecraft', 'netherite', true], 25: ['minecraft', 'netherite', true],
+    26: ['minecraft', 'netherite', true], 27: ['minecraft', 'netherite', true], 28: ['minecraft', 'diamond', true],
+    29: ['minecraft', 'diamond', true], 30: ['minecraft', 'diamond', true]}
 // Enchantments
 const basic_armor_enchantments = {0: ['3', 'minecraft:protection'], 1: ['4', 'minecraft:fire_protection']}
 const advanced_armor_enchantments = {0: ['4', 'minecraft:protection'], 1: ['5', 'minecraft:fire_protection']}
 // Trims
-const trim_materials = {0: 'minecraft:gold', 1: 'minecraft:copper', 2: 'minecraft:iron', 3: 'minecraft:netherite', 4: 'minecraft:emerald', 5: 'minecraft:redstone', 6: 'minecraft:diamond', 7: 'minecraft:lapis', 8: 'minecraft:quartz', 9: 'minecraft:ruby', 10: 'minecraft:amethyst'}
-const trim_patterns = {0: 'minecraft:wayfinder', 1: 'minecraft:raiser', 2: 'minecraft:shaper', 3: 'minecraft:host', 4: 'minecraft:sentry', 5: 'minecraft:vex', 6: 'minecraft:wild', 7: 'minecraft:tide', 8: 'minecraft:dune', 9: 'minecraft:coast', 10: 'minecraft:eye', 11: 'minecraft:rib', 12: 'minecraft:ward', 13: 'minecraft:silence'}
+const trim_materials = {0: 'minecraft:gold', 1: 'minecraft:copper', 2: 'minecraft:iron', 3: 'minecraft:netherite',
+    4: 'minecraft:emerald', 5: 'minecraft:redstone', 6: 'minecraft:diamond', 7: 'minecraft:lapis', 8: 'minecraft:quartz',
+    9: 'minecraft:ruby', 10: 'minecraft:amethyst'}
+const trim_patterns = {0: 'minecraft:wayfinder', 1: 'minecraft:raiser', 2: 'minecraft:shaper', 3: 'minecraft:host',
+    4: 'minecraft:sentry', 5: 'minecraft:vex', 6: 'minecraft:wild', 7: 'minecraft:tide', 8: 'minecraft:dune', 9: 'minecraft:coast',
+    10: 'minecraft:eye', 11: 'minecraft:rib', 12: 'minecraft:ward', 13: 'minecraft:silence'}
 
 function randomArmorEnchants(difficulty) {
     // The amount of enchantments to create
@@ -76,8 +98,6 @@ function randomArmorEnchants(difficulty) {
 
 // Gets randomized armor sets with random trims and enchantments
 function randomArmorItems(enemy, difficulty) {
-    var trimMat = trim_materials[Math.round(Math.random() * 10)]
-    var trimPat = trim_patterns[Math.round(Math.random() * 13)]
     var armorType = []; var enchantments = ''
 
     switch (enemy) {
@@ -86,37 +106,59 @@ function randomArmorItems(enemy, difficulty) {
         default:
             switch (difficulty) {
                 case 'basic':
-                    armorType = basic_armor[Math.round(Math.random())]
+                    armorType = basic_armor[Math.round(Math.random() * 30)]
                     enchantments = randomArmorEnchants(difficulty)
                     break
                 case 'adv':
-                    armorType = advanced_armor[Math.round(Math.random())]
+                    armorType = advanced_armor[Math.round(Math.random() * 30)]
                     enchantments = randomArmorEnchants(difficulty)
                     break
             }
             break
     }
+
+    var doTrims = armorType[2]
     var boots = `${armorType[0]}:${armorType[1]}_boots`
     var leggings = `${armorType[0]}:${armorType[1]}_leggings`
     var chestplate = `${armorType[0]}:${armorType[1]}_chestplate`
     var helmet = `${armorType[0]}:${armorType[1]}_helmet`
 
-    return `{id:"${boots}",tag:{Trim:{material:"${trimMat}",pattern:"${trimPat}"},Enchantments:[${enchantments}]},Count:1},` +
-           `{id:"${leggings}",tag:{Trim:{material:"${trimMat}",pattern:"${trimPat}"},Enchantments:[${enchantments}]},Count:1},` +
-           `{id:"${chestplate}",tag:{Trim:{material:"${trimMat}",pattern:"${trimPat}"},Enchantments:[${enchantments}]},Count:1},` +
-           `{id:"${helmet}",tag:{Trim:{material:"${trimMat}",pattern:"${trimPat}"},Enchantments:[${enchantments}]},Count:1}`
+    // Only apply armor trims to certain armor sets
+    if (doTrims) {
+        var trimMat = trim_materials[Math.round(Math.random() * 10)]
+        var trimPat = trim_patterns[Math.round(Math.random() * 13)]
+
+        return `{id:"${boots}",tag:{Trim:{material:"${trimMat}",pattern:"${trimPat}"},Enchantments:[${enchantments}]},Count:1},` +
+               `{id:"${leggings}",tag:{Trim:{material:"${trimMat}",pattern:"${trimPat}"},Enchantments:[${enchantments}]},Count:1},` +
+               `{id:"${chestplate}",tag:{Trim:{material:"${trimMat}",pattern:"${trimPat}"},Enchantments:[${enchantments}]},Count:1},` +
+               `{id:"${helmet}",tag:{Trim:{material:"${trimMat}",pattern:"${trimPat}"},Enchantments:[${enchantments}]},Count:1}`
+    } else {
+        return `{id:"${boots}",tag:{Enchantments:[${enchantments}]},Count:1},` +
+               `{id:"${leggings}",tag:{Enchantments:[${enchantments}]},Count:1},` +
+               `{id:"${chestplate}",tag:{Enchantments:[${enchantments}]},Count:1},` +
+               `{id:"${helmet}",tag:{Enchantments:[${enchantments}]},Count:1}`
+    }
 }
 
 /*
     Weapon constants
 */
-const basic_weapons = {0: 'minecraft:diamond_sword', 1: 'minecraft:diamond_axe'}
-const advanced_weapons = {0: 'minecraft:netherite_sword', 1: 'minecraft:netherite_axe'}
+const basic_weapons = {0: 'minecraft:diamond_sword', 1: 'minecraft:diamond_axe', 2: 'twilightforest:knightmetal_sword',
+    3: 'twilightforest:knightmetal_axe', 4: 'forbidden_arcanus:reinforced_deorum_sword', 5: 'forbidden_arcanus:reinforced_deorum_axe',
+    6: 'iceandfire:silver_sword', 7: 'iceandfire:silver_axe', 8: 'cataclysm:black_steel_sword', 9: 'aether:zanite_sword',
+    10: 'aether:zanite_axe', 11: 'aether:gravitite_sword', 12: 'aether:gravitite_axe'}
+const advanced_weapons = {0: 'minecraft:netherite_sword', 1: 'minecraft:netherite_axe', 2: 'twilightforest:knightmetal_sword',
+    3: 'twilightforest:knightmetal_axe', 4: 'forbidden_arcanus:reinforced_deorum_sword', 5: 'forbidden_arcanus:reinforced_deorum_axe',
+    6: 'forbidden_arcanus:draco_arcanus_sword', 7: 'forbidden_arcanus:draco_arcanus_axe', 8: 'experienceobelisk:cognitive_sword',
+    9: 'experienceobelisk:cognitive_axe', 10: 'aether:flaming_sword', 11: 'aether:holy_sword', 12: 'ancient_aether:ancient_sword'}
 // Enchantments
 const basic_weapon_enchants = {0: ['5', 'minecraft:sharpness'], 1: ['1', 'minecraft:fire_aspect']}
-const advanced_weapon_enchants = {0: ['6', 'minecraft:sharpness'], 1: ['1', 'minecraft:fire_aspect']}
-const basic_bow_enchants = {0: ['4', 'minecraft:power'], 1: ['2', 'minecraft:punch']}
-const advanced_bow_enchants = {0: ['5', 'minecraft:power'], 1: ['3', 'minecraft:punch'], 2: ['1', 'minecraft:flame']}
+const advanced_weapon_enchants = {0: ['6', 'minecraft:sharpness'], 1: ['1', 'minecraft:fire_aspect'], 2: ['1', 'minecraft:knockback']}
+const basic_bow_enchants = {0: ['4', 'minecraft:power'], 1: ['2', 'minecraft:punch'], 2: ['3', 'minecraft:unbreaking']}
+const advanced_bow_enchants = {0: ['5', 'minecraft:power'], 1: ['3', 'minecraft:punch'], 2: ['1', 'minecraft:flame'], 3: ['3', 'minecraft:unbreaking']}
+const basic_crossbow_enchants = {0: ['1', 'minecraft:quick_charge'], 1: ['2', 'minecraft:piercing'], 2: ['3', 'minecraft:unbreaking']}
+const advanced_crossbow_enchants = {0: ['3', 'minecraft:quick_charge'], 1: ['4', 'minecraft:piercing'], 2: ['3', 'minecraft:unbreaking'],
+    3: ['1', 'minecraft:multishot']}
 
 // Gets random enchantments for weapons. Has unique output for special weapons like bows
 function randomWeaponEnchants(weaponType, difficulty) {
@@ -130,13 +172,13 @@ function randomWeaponEnchants(weaponType, difficulty) {
         case 'bow':
             switch (difficulty) {
                 case 'basic':
-                    enchData = basic_bow_enchants[Math.round(Math.random())]
+                    enchData = basic_bow_enchants[Math.round(Math.random() * 2)]
                     enchantments = `{lvl:${enchData[0]}s,id:"${enchData[1]}"}`
                     break
                 case 'adv':
-                    enchData = advanced_bow_enchants[Math.round(Math.random() * 2)]
-                    enchantments = `{lvl:${enchData[0]}s,id:"${enchData[1]}"}`
-                    enchData = advanced_bow_enchants[Math.round(Math.random() * 2)]
+                    enchData = advanced_bow_enchants[Math.round(Math.random() * 3)]
+                    enchantments = `{lvl:${enchData[0]}s,id:"${enchData[1]}"},`
+                    enchData = advanced_bow_enchants[Math.round(Math.random() * 3)]
                     enchantments += `{lvl:${enchData[0]}s,id:"${enchData[1]}"}`
                     break
             }
@@ -145,11 +187,14 @@ function randomWeaponEnchants(weaponType, difficulty) {
         case 'crossbow':
             switch (difficulty) {
                 case 'basic':
-                    enchData = basic_crossbow_enchants[Math.round(Math.random())]
+                    enchData = basic_crossbow_enchants[Math.round(Math.random() * 2)]
                     enchantments = `{lvl:${enchData[0]}s,id:"${enchData[1]}"}`
                     break
                 case 'adv':
-
+                    enchData = advanced_crossbow_enchants[Math.round(Math.random() * 3)]
+                    enchantments = `{lvl:${enchData[0]}s,id:"${enchData[1]}"},`
+                    enchData = advanced_crossbow_enchants[Math.round(Math.random() * 3)]
+                    enchantments += `{lvl:${enchData[0]}s,id:"${enchData[1]}"}`
                     break
             }
             break
@@ -161,9 +206,9 @@ function randomWeaponEnchants(weaponType, difficulty) {
                     enchantments = `{lvl:${enchData[0]}s,id:"${enchData[1]}"}`
                     break
                 case 'adv':
-                    enchData = advanced_weapon_enchants[Math.round(Math.random())]
+                    enchData = advanced_weapon_enchants[Math.round(Math.random() * 2)]
                     enchantments = `{lvl:${enchData[0]}s,id:"${enchData[1]}"},`
-                    enchData = advanced_weapon_enchants[Math.round(Math.random())]
+                    enchData = advanced_weapon_enchants[Math.round(Math.random() * 2)]
                     enchantments += `{lvl:${enchData[0]}s,id:"${enchData[1]}"}`
                     break
             }
@@ -177,28 +222,83 @@ function randomHandItems(enemy, difficulty) {
     var mainHandItem = ''; var offHandItem = ''; var enchantments = ''
 
     switch (enemy) {
-        case 'skeleton':
+        case 'bow':
             // Bows and arrows
             mainHandItem = 'minecraft:bow'
             offHandItem = 'minecraft:arrow'
-
+            enchantments = randomWeaponEnchants('bow', difficulty)
             break
 
-        case 'pillager':
+        case 'crossbow':
             // Crossbows
+            mainHandItem = 'minecraft:crossbow'
+            offHandItem = 'minecraft:air'
+            enchantments = randomWeaponEnchants('crossbow', difficulty)
+            break
 
+        case 'axe':
+            // Always an iron or diamond axe
+            switch(difficulty) {
+                case 'basic':
+                    mainHandItem = 'minecraft:iron_axe'
+                    offHandItem = 'minecraft:air'
+                    enchantments = 'none'
+                    break
+                case 'adv':
+                    mainHandItem = 'minecraft:diamond_axe'
+                    offHandItem = 'minecraft:air'
+                    enchantments = 'none'
+                    break
+            }
+            break
+
+        case 'shield':
+            // Always a vanilla shield
+            mainHandItem = 'minecraft:shield'
+            offHandItem = 'minecraft:air'
+            enchantments = 'none'
+            break
+
+        case 'cleaver':
+            // Savage & Ravage Cleaver of Beheading
+            mainHandItem = 'savage_and_ravage:cleaver_of_beheading'
+            offHandItem = 'minecraft:air'
+            enchantments = randomWeaponEnchants('generic', difficulty)
+            break
+
+        case 'hatchet':
+            // Platinum Hatchet
+            mainHandItem = 'illagerinvasion:platinum_infused_hatchet'
+            offHandItem = 'minecraft:air'
+            enchantments = 'none'
+            break
+
+        case 'dual':
+            // Generic melee items but with a shield
+            switch(difficulty) {
+                case 'basic':
+                    mainHandItem = basic_weapons[Math.round(Math.random() * 12)]
+                    offHandItem = 'minecraft:shield'
+                    enchantments = randomWeaponEnchants('generic', difficulty)
+                    break
+                case 'adv':
+                    mainHandItem = advanced_weapons[Math.round(Math.random() * 12)]
+                    offHandItem = 'minecraft:shield'
+                    enchantments = randomWeaponEnchants('generic', difficulty)
+                    break
+            }
             break
 
         default:
             // Generic melee items
             switch(difficulty) {
                 case 'basic':
-                    mainHandItem = basic_weapons[Math.round(Math.random())]
+                    mainHandItem = basic_weapons[Math.round(Math.random() * 12)]
                     offHandItem = 'minecraft:air'
                     enchantments = randomWeaponEnchants('generic', difficulty)
                     break
                 case 'adv':
-                    mainHandItem = advanced_weapons[Math.round(Math.random())]
+                    mainHandItem = advanced_weapons[Math.round(Math.random() * 12)]
                     offHandItem = 'minecraft:air'
                     enchantments = randomWeaponEnchants('generic', difficulty)
                     break
--- kubejs/server_scripts/server_commands/summon_enemies.js
+++ kubejs/server_scripts/server_commands/summon_enemies.js
@@ -2,13 +2,21 @@
     Enemy constants
 */
 const zombie_types = {0: 'minecraft:zombie', 1: 'minecraft:husk', 2: 'minecraft:drowned'}
-const skeleton_types = {0: 'minecraft:skeleton', 1: 'minecraft:stray', 2: 'minecraft:wither_skeleton'}
-const insect_types = {0: 'minecraft:spider', 1: 'minecraft:cave_spider', 2: 'twilightforest:hedge_spider', 3: 'tropicraft:tropi_spider'}
-const ranged_illager_types = {0: 'minecraft:pillager', 1: 'minecraft:evoker'}
-const melee_illager_types = {0: 'minecraft:vindicator', 1: 'illagerinvasion:inquisitor', 2: 'illagerinvasion:marauder'}
-const mage_types = {0: 'minecraft:evoker', 1: 'minecraft:witch', 2: 'irons_spellbooks:archevoker', 3: 'irons_spellbooks:pyromancer', 4: 'illagerinvasion:alchemist'}
-const elemental_types = {0: 'minecraft:blaze', 1: 'call_of_yucutan:blowgun_huracan'}
-const slime_types = {0: 'minecraft:slime', 1: 'alexsmobs:mimicube'}
+const skeleton_types = {0: 'minecraft:skeleton', 1: 'minecraft:stray', 2: 'minecraft:wither_skeleton', 3: 'savage_and_ravage:skeleton_villager'}
+const insect_types = {0: 'minecraft:spider', 1: 'minecraft:cave_spider', 2: 'twilightforest:hedge_spider', 3: 'tropicraft:tropi_spider',
+    4: 'twilightforest:fire_beetle', 5: 'twilightforest:slime_beetle', 6: 'twilightforest:pinch_beetle', 7: 'minecraft:spider',
+    8: 'minecraft:spider'}
+const ranged_illager_types = {0: 'minecraft:pillager', 1: 'minecraft:illusioner', 2: 'illagerinvasion:alchemist', 3: 'illagerinvasion:archivist',
+    4: 'minecraft:pillager', 5: 'minecraft:pillager', 6: 'illagerinvasion:provoker', 7: 'illagerinvasion:marauder'}
+const melee_illager_types = {0: 'minecraft:vindicator', 1: 'illagerinvasion:inquisitor', 2: 'illagerinvasion:basher',
+    3: 'illagerinvasion:basher', 4: 'savage_and_ravage:executioner', 5: 'minecraft:vindicator'}
+const mage_types = {0: 'minecraft:evoker', 1: 'minecraft:witch', 2: 'irons_spellbooks:archevoker', 3: 'irons_spellbooks:pyromancer',
+    4: 'irons_spellbooks:cryomancer', 5: 'illagerinvasion:alchemist', 6: 'illagerinvasion:archivist', 7: 'illagerinvasion:firecaller',
+    8: 'illagerinvasion:sorcerer', 9: 'illagerinvasion:necromancer', 10: 'savage_and_ravage:trickster'}
+const elemental_types = {0: 'minecraft:blaze', 1: 'call_of_yucutan:blowgun_huracan', 2: 'minecraft:blaze', 3: 'twilightforest:death_tome',
+    4: 'aether:fire_minion'}
+const slime_types = {0: 'minecraft:slime', 1: 'alexsmobs:mimicube', 2: 'twilightforest:maze_slime', 3: 'the_bumblezone:honey_slime',
+    4: 'aether_redux:vanilla_swet', 5: 'aether:blue_swet', 6: 'aether:sentry', 7: 'minecraft:slime'}
 const wilden_types = {0: 'ars_nouveau:wilden_guardian', 1: 'ars_nouveau:wilden_hunter', 2: 'ars_nouveau:wilden_stalker'}
 
 /*
@@ -18,7 +26,7 @@ function summonZombie(server, dimension, difficulty, x, y, z) {
     var zombieType = zombie_types[Math.round(Math.random() * 2)]
     var effect = randomPositiveEffect(difficulty)
     var hp = Math.round((Math.random() + 1.6) * 10)
-    var handItems = randomHandItems('zombie', difficulty)
+    var handItems = randomHandItems('generic', difficulty)
     var armorItems = randomArmorItems('zombie', difficulty)
     
     var cmd = `execute in ${dimension} run summon ${zombieType} ${x} ${y} ${z} ` +
@@ -32,7 +40,7 @@ function summonZombie(server, dimension, difficulty, x, y, z) {
     Summons a skeleton with buffed attributes and random equipment
 */
 function summonSkeleton(server, dimension, difficulty, x, y, z) {
-    var skeletonType = skeleton_types[Math.round(Math.random() * 2)]
+    var skeletonType = skeleton_types[Math.round(Math.random() * 3)]
     var effect = randomPositiveEffect(difficulty)
     var hp = Math.round((Math.random() + 1.4) * 10)
     var armorItems = randomArmorItems('skeleton', difficulty)
@@ -40,9 +48,11 @@ function summonSkeleton(server, dimension, difficulty, x, y, z) {
     // Give either melee weapons or bows
     var handItems;
     if (skeletonType == 'minecraft:wither_skeleton')
-        handItems = randomHandItems('wither_skeleton', difficulty)
+        handItems = randomHandItems('generic', difficulty)
+    else if (skeletonType == 'savage_and_ravage:skeleton_villager')
+        handItems = randomHandItems('crossbow', difficulty)
     else
-        handItems = randomHandItems('skeleton', difficulty)
+        handItems = randomHandItems('bow', difficulty)
 
     var cmd = `execute in ${dimension} run summon ${skeletonType} ${x} ${y} ${z} ` +
               `{Health:${hp},PersistenceRequired:1,ActiveEffects:[${effect}],` +
@@ -81,12 +91,33 @@ function summonCreeper(server, dimension, difficulty, x, y, z) {
     Summons a Ranged Illager with buffed attributes
 */
 function summonRangedIllager(server, dimension, difficulty, x, y, z) {
-    var illagerType = ranged_illager_types[Math.round(Math.random())]
+    var illagerType = ranged_illager_types[Math.round(Math.random() * 7)]
     var effect = randomPositiveEffect(difficulty)
     var hp = Math.round((Math.random() + 1.4) * 10)
+    var handItems
 
+    switch (illagerType) {
+        case 'minecraft:pillager':
+            handItems = randomHandItems('crossbow', difficulty)
+            break
+        case 'minecraft:illusioner':
+            handItems = randomHandItems('bow', difficulty)
+            break
+        case 'illagerinvasion:alchemist':
+            handItems = randomHandItems('bow', difficulty)
+            break
+        case 'illagerinvasion:provoker':
+            handItems = randomHandItems('bow', difficulty)
+            break
+        case 'illagerinvasion:marauder':
+            handItems = randomHandItems('hatchet', difficulty)
+            break
+        default:
+            handItems = '{id:"minecraft:air",Count:1}'
+    }
     var cmd = `execute in ${dimension} run summon ${illagerType} ${x} ${y} ${z} ` +
               `{Health:${hp},PersistenceRequired:1,ActiveEffects:[${effect}],` +
+              `HandItems:[${handItems}],HandDropChances:[0f,0f],` +
               `Attributes:[{Name:"generic.armor",Base:4},{Name:"generic.max_health",Base:${hp}}]}`
     server.runCommandSilent(cmd)
 }
@@ -95,9 +126,9 @@ function summonRangedIllager(server, dimension, difficulty, x, y, z) {
     Summons a Melee Illager with buffed attributes
 */
 function summonMeleeIllager(server, dimension, difficulty, x, y, z) {
-    var illagerType = melee_illager_types[Math.round(Math.random() * 2)]
+    var illagerType = melee_illager_types[Math.round(Math.random() * 5)]
     var effect = randomPositiveEffect(difficulty)
-    var hp;
+    var hp; var handItems
     switch (difficulty) {
         case 'basic':
             hp = Math.round((Math.random() + 1.6) * 10)
@@ -106,9 +137,27 @@ function summonMeleeIllager(server, dimension, difficulty, x, y, z) {
             hp = Math.round((Math.random() + 2) * 10)
             break
     }
+    switch (illagerType) {
+        case 'minecraft:vindicator':
+            handItems = randomHandItems('axe', difficulty)
+            break
+        case 'illagerinvasion:basher':
+            handItems = randomHandItems('shield', difficulty)
+            break
+        case 'savage_and_ravage:executioner':
+            handItems = randomHandItems('cleaver', difficulty)
+            break
+        case 'illagerinvasion:inquisitor':
+            handItems = randomHandItems('dual', difficulty)
+            break
+        default:
+            handItems = randomHandItems('generic', difficulty)
+            break
+    }
 
     var cmd = `execute in ${dimension} run summon ${illagerType} ${x} ${y} ${z} ` +
               `{Health:${hp},PersistenceRequired:1,ActiveEffects:[${effect}],` +
+              `HandItems:[${handItems}],HandDropChances:[0f,0f],` +
               `Attributes:[{Name:"generic.armor",Base:3},{Name:"generic.max_health",Base:${hp}}]}`
     server.runCommandSilent(cmd)
 }
@@ -117,18 +166,25 @@ function summonMeleeIllager(server, dimension, difficulty, x, y, z) {
     Summons a Mage with buffed attributes
 */
 function summonMage(server, dimension, difficulty, x, y, z) {
-    var mageType = mage_types[Math.round(Math.random() * 4)]
+    var mageType = mage_types[Math.round(Math.random() * 10)]
     var effect = randomPositiveEffect(difficulty)
     var hp = Math.round((Math.random() + 1.4) * 10)
-    var armorItems;
+    var armorItems; var armorAttribute = 4;
     switch (mageType) {
         case 'irons_spellbooks:archevoker':
             armorItems = '{id:"irons_spellbooks:archevoker_boots",Count:1},{id:"irons_spellbooks:archevoker_leggings",Count:1},' +
                          '{id:"irons_spellbooks:archevoker_chestplate",Count:1},{id:"irons_spellbooks:archevoker_helmet",Count:1}'
+            armorAttribute = 2
             break
         case 'irons_spellbooks:pyromancer':
             armorItems = '{id:"irons_spellbooks:pyromancer_boots",Count:1},{id:"irons_spellbooks:pyromancer_leggings",Count:1},' +
                          '{id:"irons_spellbooks:pyromancer_chestplate",Count:1},{id:"irons_spellbooks:pyromancer_helmet",Count:1}'
+            armorAttribute = 2
+            break
+        case 'irons_spellbooks:cryomancer':
+            armorItems = '{id:"irons_spellbooks:cryomancer_boots",Count:1},{id:"irons_spellbooks:cryomancer_leggings",Count:1},' +
+                         '{id:"irons_spellbooks:cryomancer_chestplate",Count:1},{id:"irons_spellbooks:cryomancer_helmet",Count:1}'
+            armorAttribute = 2
             break
         default:
             armorItems = '{id:"minecraft:air"},{id:"minecraft:air"},{id:"minecraft:air"},{id:"minecraft:air"}'
@@ -137,7 +193,7 @@ function summonMage(server, dimension, difficulty, x, y, z) {
     var cmd = `execute in ${dimension} run summon ${mageType} ${x} ${y} ${z} ` +
               `{Health:${hp},PersistenceRequired:1,ActiveEffects:[${effect}],` +
               `ArmorItems:[${armorItems}],ArmorDropChances:[0f,0f,0f,0f],` +
-              `Attributes:[{Name:"generic.armor",Base:4},{Name:"generic.max_health",Base:${hp}}]}`
+              `Attributes:[{Name:"generic.armor",Base:${armorAttribute}},{Name:"generic.max_health",Base:${hp}}]}`
     server.runCommandSilent(cmd)
 }
 
@@ -145,7 +201,7 @@ function summonMage(server, dimension, difficulty, x, y, z) {
     Summons an insect with buffed attributes.
 */
 function summonInsect(server, dimension, difficulty, x, y, z) {
-    var insectType = insect_types[Math.round(Math.random() * 3)]
+    var insectType = insect_types[Math.round(Math.random() * 8)]
     var effect = randomPositiveEffect(difficulty)
     var hp = Math.round((Math.random() + 1) * 10)
 
@@ -159,7 +215,7 @@ function summonInsect(server, dimension, difficulty, x, y, z) {
     Summons a Slime with buffed attributes.
 */
 function summonSlime(server, dimension, difficulty, x, y, z) {
-    var slimeType = slime_types[Math.round(Math.random())]
+    var slimeType = slime_types[Math.round(Math.random() * 7)]
     var effect = randomPositiveEffect(difficulty)
     
     var cmd = `execute in ${dimension} run summon ${slimeType} ${x} ${y} ${z} ` +
@@ -171,7 +227,7 @@ function summonSlime(server, dimension, difficulty, x, y, z) {
     Summons an elemental with buffed attributes.
 */
 function summonElemental(server, dimension, difficulty, x, y, z) {
-    var elementalType = elemental_types[Math.round(Math.random())]
+    var elementalType = elemental_types[Math.round(Math.random() * 4)]
     var effect = randomPositiveEffect(difficulty)
     var hp = Math.round((Math.random() + 1.5) * 10)
 
